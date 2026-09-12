982937e8ff91f9f0a9be87cb409b80c763040ab2
C4TA CIUDADELA <estudiante@iMac-de-estudiante-7.local>
2026-09-11 20:40:54 -0400

    fix: chang vitest.config lcov

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ test: {
     include: ["test/**/*.spec.ts"],
     coverage: {
         provider: "v8",
-        reporter: ["text", "html"],
+        reporter: ["text", "html","lcov"],
         include: ["src/**/*.ts"],
         exclude: [
         ...defaultExclude,
